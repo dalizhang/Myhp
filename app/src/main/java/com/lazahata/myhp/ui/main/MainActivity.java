31b87355dcaf9f3d077fcca325b8a9b96556066e
dalizhang <dalizhang@foxmail.com>
2017-02-07 15:34:05 +0800

    remove native code

--- app/src/main/java/com/lazahata/myhp/ui/main/MainActivity.java
+++ app/src/main/java/com/lazahata/myhp/ui/main/MainActivity.java
@@ -46,14 +46,4 @@ public class MainActivity extends FragmentActivity {
         binding.viewpager.setCurrentItem(3);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
